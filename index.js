c6f8fa38156e48896e83ebf3cd541617f1850ac7
janeishahu <janeishahu@aol.com>
2021-09-24 22:55:11 -0400

    frontend and backend are running

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.get('/genres/:id', getGenreById)
 app.get('/api/novels', getAllNovels)
 app.get('/api/novels/:identifier', getNovelByIdOrTitle)
 
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client/build', 'index.html')))
+
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
 })

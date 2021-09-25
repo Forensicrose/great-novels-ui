393fca1f334fb7eef7e904e14fa20503be54d024
janeishahu <janeishahu@aol.com>
2021-09-24 22:59:22 -0400

    updated index.js file

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const { getAllAuthors, getAuthorByIdOrName } = require('./controllers/authors')
 const { getAllGenres, getGenreById } = require('./controllers/genres')
 const { getAllNovels, getNovelByIdOrTitle } = require('./controllers/novels')
 
 const app = express()
+
 app.use(cors())
+app.use(express.static('client/build'))
 
 app.get('/api/authors', getAllAuthors)
-app.get('/authors/:identifier', getAuthorByIdOrName)
+app.get('/api/authors/:identifier', getAuthorByIdOrName)
 
 app.get('/api/genres', getAllGenres)
-app.get('/genres/:id', getGenreById)
+app.get('api/genres/:id', getGenreById)
 
 app.get('/api/novels', getAllNovels)
 app.get('/api/novels/:identifier', getNovelByIdOrTitle)
